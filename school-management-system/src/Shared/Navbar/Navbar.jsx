800393a846eaae7d41f16f11edf8082439b20f3d
Hasib <hhasibul231@gmail.com>
2023-09-05 09:06:30 +0600

    slider added

--- school-management-system/src/Shared/Navbar/Navbar.jsx
+++ school-management-system/src/Shared/Navbar/Navbar.jsx
@@ -1,8 +1,10 @@
 import React from "react";
-
+import { Link } from "react-router-dom";
+import { FaAngleDown } from "react-icons/fa";
 const Navbar = () => {
+    
   return (
-    <div className="navbar bg-slate-200">
+    <div className="navbar bg-base-200">
       {/* mobile navbar */}
       <div className="navbar-start">
         <div className="dropdown ">
@@ -24,7 +26,7 @@ const Navbar = () => {
           </label>
           <ul
             tabIndex={0}
-            className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52"
+            className="menu menu-sm z-10 dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52"
           >
             <li>
               <a>Item 1</a>
@@ -45,37 +47,93 @@ const Navbar = () => {
             </li>
           </ul>
         </div>
-        <a className="btn btn-ghost normal-case text-xl">daisyUI</a>
+        <a className="btn btn-ghost normal-case text-xl">School Name</a>
       </div>
 
       {/* Main navbar */}
       <div className="navbar-center hidden lg:flex">
-        <ul className="menu menu-horizontal px-1">
+        <ul className="menu menu-horizontal px-1 z-10">
           <li>
-            <a>Item 1</a>
+            <Link to="/">Home</Link>
           </li>
 
-          {/* Hover dropdown menu */}
           <li className="dropdown dropdown-hover">
-            <a>Parent</a>
-            <ul className="p-2 dropdown-content shadow bg-base-100 rounded-box w-52">
+            <a>
+              About <FaAngleDown></FaAngleDown>
+            </a>
+            <ul className="p-2 dropdown-content shadow bg-base-100 rounded-box w-48">
               <li>
-                <a>Submenu 1</a>
+                <Link to="/">About Us</Link>
               </li>
               <li>
-                <a>Submenu 2</a>
+                <Link to="/">Acceptances Certificate</Link>
               </li>
               <li>
-                <a>Submenu 1</a>
+                <Link to="/">National Certificate</Link>
+              </li>
+            </ul>
+          </li>
+
+          <li className="dropdown dropdown-hover">
+            <a>
+              Administration <FaAngleDown></FaAngleDown>
+            </a>
+            <ul className="p-2 dropdown-content shadow bg-base-100 rounded-box w-48">
+              <li>
+                <Link to="/">Chairman</Link>
               </li>
               <li>
-                <a>Submenu 2</a>
+                <Link to="/">Principal</Link>
+              </li>
+              <li>
+                <Link to="/">Governing</Link>
+              </li>
+              <li>
+                <Link to="/">Staff</Link>
+              </li>
+              <li>
+                <Link to="/">Rules & Regulation</Link>
+              </li>
+            </ul>
+          </li>
+
+          <li className="dropdown dropdown-hover">
+            <a>
+              Academic <FaAngleDown></FaAngleDown>
+            </a>
+            <ul className="p-2 dropdown-content shadow bg-base-100 rounded-box w-48">
+              <li>
+                <Link to="/">Admission</Link>
+              </li>
+              <li>
+                <Link to="/">Fees</Link>
+              </li>
+              <li>
+                <Link to="/">Ebook</Link>
+              </li>
+              <li>
+                <Link to="/">Class Routine</Link>
+              </li>
+              <li>
+                <Link to="/">Exam Routine</Link>
+              </li>
+              <li>
+                <Link to="/">Syllabus</Link>
+              </li>
+              <li>
+                <Link to="/">Result</Link>
               </li>
             </ul>
           </li>
 
           <li>
-            <a>Item 3</a>
+            <Link to="/">Students</Link>
+          </li>
+          <li>
+            <Link to="/">Gallery</Link>
+          </li>
+          <li>
+            <Link to="/">Contact</Link>
           </li>
         </ul>
       </div>
---
+++ school-management-system/src/pages/Home/Slider.jsx
@@ -0,0 +1,53 @@
+import React, { useRef, useState } from "react";
+import { Swiper, SwiperSlide } from "swiper/react";
+
+import "swiper/css";
+import "swiper/css/pagination";
+import "swiper/css/navigation";
+
+import { Autoplay, Pagination, Navigation } from "swiper";
+
+const Slider = () => {
+  return (
+    <div className="w-10/12 mx-auto mb-24">
+      <Swiper
+        spaceBetween={30}
+        centeredSlides={true}
+        autoplay={{
+          delay: 2500,
+          disableOnInteraction: false,
+        }}
+        pagination={{
+          clickable: true,
+        }}
+        navigation={true}
+        modules={[Autoplay, Pagination, Navigation]}
+        className="mySwiper"
+      >
+        <SwiperSlide>
+          <img
+            className="w-full"
+            src="https://img.freepik.com/free-photo/html-css-collage-concept-with-person_23-2150062008.jpg?w=1380&t=st=1689041921~exp=1689042521~hmac=d8a5f9a905126b68366892416fbd1c3ddf2aa510236daf253e782cca3fafb872"
+            alt=""
+          />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img
+            className="w-full"
+            src="https://img.freepik.com/free-photo/rear-view-programmer-working-all-night-long_1098-18697.jpg?w=1380&t=st=1689042021~exp=1689042621~hmac=175a767f7fb1cbfe6381ff4a660c5d057ed2da5c42f0586b10ddfa0f00056acb"
+            alt=""
+          />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img
+            className="w-full "
+            src="https://img.freepik.com/free-photo/html-system-website-concept_23-2150376760.jpg?w=1380&t=st=1689042101~exp=1689042701~hmac=a25a1db487189f453521c4c3718323c4b80873244fccecfb7a930cf7bcbfb5ef"
+            alt=""
+          />
+        </SwiperSlide>
+      </Swiper>
+    </div>
+  );
+};
+
+export default Slider;
