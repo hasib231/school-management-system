d1e1b0354dbcc69a156d2e60b2e5fff8c05d5239
sohel-ashik <sohelashik594@gmail.com>
2023-09-07 02:14:41 +0600

    x

--- school-management-system/src/components/personCard/PersonDetailsCard.jsx
+++ school-management-system/src/components/personCard/PersonDetailsCard.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const PersonDetailsCard = ({imgLink,name,designation}) => {
     return (
         <div className='w-full pl-2 pr-2 lg:w-9/12'>
-            <div className="card w-full bg-base-100 shadow-xl">
+            <div className="card w-fit bg-base-100 shadow-xl">
                 <figure ><img className='md:h-48 md:w-48 md:object-cover' src={imgLink} alt="Shoes" /></figure>
                 <div className="card-body">
                     <h2 className="card-title">{name}</h2>
--- school-management-system/src/pages/Administration/Governings.jsx
+++ school-management-system/src/pages/Administration/Governings.jsx
@@ -6,7 +6,7 @@ const Governing = () => {
     const adminData = data.governing;
     return (
         <div className='mt-5 flex flex-col pb-10 pt-5'>
-            <div className='grid justify-items-center  grid-cols-1  gap-y-10 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3'>
+            <div className='grid justify-items-center   grid-cols-1  gap-y-10 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3'>
                 {adminData.map((ele) => (
                 <PersonDetailsCard imgLink={ele.imgLink} name={ele.name} designation={ele.designation} />
                 ))}
