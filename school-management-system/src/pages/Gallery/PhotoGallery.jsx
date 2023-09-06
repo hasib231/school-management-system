ae5b7effa9bd38d01b9755b1a69054ead5634444
Hasib <hhasibul231@gmail.com>
2023-09-07 00:43:41 +0600

    Gallery updated

--- school-management-system/src/pages/Gallery/PhotoGallery.jsx
+++ school-management-system/src/pages/Gallery/PhotoGallery.jsx
@@ -25,16 +25,16 @@ const PhotoGallery = () => {
   return (
     <div className="container mx-auto px-20 py-12">
       <h1 className="text-4xl font-bold text-center mb-8">Image Gallery</h1>
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5">
         {images.map((image, index) => (
           <div
             key={index}
-            className="rounded-lg"
+            className="rounded-lg transform hover:scale-105 hover:ease-in duration-200 shadow-lg"
             onClick={() => handleOpenModal(image)}
           >
             <figure>
               <img
-                className="rounded-lg"
+                className="rounded-lg h-72 w-full"
                 src={image}
                 alt={`Image ${index + 1}`}
               />
