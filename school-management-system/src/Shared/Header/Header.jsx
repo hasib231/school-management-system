9928f1fce0a5c00b942ba864017ea941d94bad87
Hasib <hhasibul231@gmail.com>
2023-09-07 06:41:55 +0600

    notice board added

--- school-management-system/src/Shared/Header/Header.jsx
+++ school-management-system/src/Shared/Header/Header.jsx
@@ -5,12 +5,14 @@ const Header = () => {
     return (
       <div className="flex ms-28 py-2">
         <div>
-          <img className="w-24" src={logo_sm} alt="" />
+          <img className="w-14 md:w-24" src={logo_sm} alt="" />
         </div>
         <div className="ms-3">
-          <h6 className="font-semibold">ESTD: 2015</h6>
-          <h1 className="font-medium text-3xl">Kanthal Bari High School</h1>
-          <h6 className="font-semibold">Ulipur, Kurigram</h6>
+          <h6 className="text-xs md:font-semibold">ESTD: 2015</h6>
+          <h1 className="font-medium text-1xl md:text-3xl">
+            Kanthal Bari High School
+          </h1>
+          <h6 className="text-xs md:font-semibold">Ulipur, Kurigram</h6>
         </div>
       </div>
     );
