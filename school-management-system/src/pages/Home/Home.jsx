9090e11cb2b6f5567eae7a94a10c06dbb8ca03b6
sohel-ashik <sohelashik594@gmail.com>
2023-09-08 22:57:19 +0600

    x

--- school-management-system/src/pages/Home/Home.jsx
+++ school-management-system/src/pages/Home/Home.jsx
@@ -14,10 +14,7 @@ const Home = () => {
             <Slider></Slider>
             <TotalLeaderCard></TotalLeaderCard>
             <AboutUs></AboutUs>
-<<<<<<< HEAD
-=======
             <TeachersCardSlide></TeachersCardSlide>
->>>>>>> 2672d1630b06de8e3bd15ea031094e5be209441c
             <OfficialLink></OfficialLink>
             <Contacts></Contacts>
       </div>
