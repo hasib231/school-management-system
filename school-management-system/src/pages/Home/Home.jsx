6509fc81da9170e2daaa7b1be1a3f9a596d65a2f
Hasib <hhasibul231@gmail.com>
2023-09-08 22:14:53 +0600

    add official link

--- school-management-system/src/pages/Home/Home.jsx
+++ school-management-system/src/pages/Home/Home.jsx
@@ -13,6 +13,7 @@ const Home = () => {
             <Slider></Slider>
             <TotalLeaderCard></TotalLeaderCard>
             <AboutUs></AboutUs>
+             <OfficialLink></OfficialLink>
             <Contacts></Contacts>
       </div>
     );
