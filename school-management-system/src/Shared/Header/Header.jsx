ce52a883ad877698a66286a1c8513398df642356
sohel-ashik <sohelashik594@gmail.com>
2023-09-08 23:21:48 +0600

    x

--- school-management-system/src/Shared/Header/Header.jsx
+++ school-management-system/src/Shared/Header/Header.jsx
@@ -6,14 +6,14 @@ const Header = () => {
       <div className='flex justify-between'>
         <div className="flex md:ms-28 py-2 pl-10 md:pl-0 ">
             <div>
-            <img className="w-14 md:w-24" src={logo_sm} alt="" />
+            <img className="w-16 md:w-28" src={logo_sm} alt="" />
             </div>
-            <div className="ms-3">
-            <h1 className="font-bold md:font-medium text-1xl md:text-3xl">
-                Kanthal Bari High School
-            </h1>
-            <h6 className="text-sm font-semibold">Ulipur, Kurigram</h6>
-            <h6 className="text-xs md:font-semibold">ESTD: 2015</h6>
+            <div className="ms-3 flex flex-col gap-1">
+                <h1 className="font-bold md:font-medium text-1xl md:text-3xl">
+                    কাঁঠাল বাড়ী উচ্চ বিদ্যালয়
+                </h1>
+                <h6 className="text-sm font-semibold">জুম্মাহাট,উলিপুর,কুড়িগ্রাম</h6>
+                <h6 className="text-xs md:font-semibold">স্থাপিতঃ ১৯৪৬</h6>
             </div>
         </div>
         <div className='text-lg  lg:flex flex-col text-right pt-3 pr-24 hidden'>
